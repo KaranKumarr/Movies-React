c74fd0b21f8ae4f454121564b7539636975ce443
Karan Kumar <karankumar0334@gmail.com>
2022-07-23 21:41:55 +0500

    Routes fixed

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
+import { Routes, Route, Navigate } from 'react-router-dom';
+// import { AnimatePresence } from 'framer-motion';
 import Movies from './Movies';
 import TvShows from './TvShows';
 import MovieDetail from './MovieDetail';
@@ -11,20 +11,20 @@ import TvShowDetail from './TvShowDetail';
 
 function Pages() {
 
-    const location = useLocation();
+    // const location = useLocation();
 
     return (
-        <AnimatePresence exitBeforeEnter>
-            <Routes location={location} key={location.pathname}>
-                <Route path='/' element={<Movies />} />
-                <Route path='/Movies-React' element={<Navigate to={'/'} />} />
-                <Route path='movie/:movieId' element={<MovieDetail />} />
-                <Route path='/tv' element={<TvShows />} />
-                <Route path='search/movies/:searchTerm' element={<SearchMovies />} />
-                <Route path='search/tv/:searchTerm' element={<SearchTvShows />} />
-                <Route path='tv/:tvShowId' element={<TvShowDetail />} />
-            </Routes>
-        </AnimatePresence>
+        // <AnimatePresence exitBeforeEnter>
+        <Routes>
+            <Route path='/' element={<Movies />} />
+            <Route path='/Movies-React' element={<Navigate to={'/'} />} />
+            <Route path='movie/:movieId' element={<MovieDetail />} />
+            <Route path='/tv' element={<TvShows />} />
+            <Route path='search/movies/:searchTerm' element={<SearchMovies />} />
+            <Route path='search/tv/:searchTerm' element={<SearchTvShows />} />
+            <Route path='tv/:tvShowId' element={<TvShowDetail />} />
+        </Routes>
+        // </AnimatePresence>
     );
 }
 
